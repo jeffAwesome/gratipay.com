1e5bec09bb9a83a1e114d4555efbe363529b67f4
Chad Whitacre <chad@zetaweb.com>
2016-05-09 21:12:25 -0400

    Add a helper to Country; should go upstream prolly

--- gratipay/models/country.py
+++ gratipay/models/country.py
@@ -16,3 +16,7 @@ class Country(Model):
 
     """
     typname = 'countries'
+
+    @classmethod
+    def from_code2(cls, code2):
+        return cls.db.one("SELECT countries.*::countries FROM countries WHERE code2=%s", (code2,))
